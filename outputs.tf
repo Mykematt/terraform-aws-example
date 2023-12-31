6ca717721be1f76012a860cc65618f0e28e6b1b8
Mykematt <122421851+Mykematt@users.noreply.github.com>
2023-12-31 07:22:50 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,3 @@ output "ec2_instance_id" {
   description = "This is the AWS ID of the EC2 instance we created."
 }
 
-output "ec2_instance_id" {
-  value       = aws_instance.web_2.id
-  description = "This is the AWS ID of the EC2 instance #2 we created."
-}
